93de5de2939287232dad2057c2eb7c7fd274e1be
Chris Mendez <mendecj812@gmail.com>
2025-12-17 16:25:47 -0600

    feat: home route fetches all theaters from neon db

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,42 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router";
 import { db } from "~/db/db";
-import { auditorium } from "~/db/schema";
+import { auditorium, theater } from "~/db/schema";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Movie Seat Tracker" },
+    { name: "description", content: "Track your seats!" },
   ];
 }
 
 export async function loader() {
-  console.log(await db.select().from(auditorium).execute());
+  // console.log(await db.select().from(theater));
+  const data = await db.select().from(theater);
+  return data;
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return (
+    <main className="max-w-80 mx-auto grid gap-6 pt-16 pb-4">
+      <div>
+        <h1 className="text-5xl font-black">Movie Seat Tracker</h1>
+      </div>
+      <h2 className="font-bold text-3xl">Your theaters:</h2>
+
+      {loaderData.map(theater => {
+        return (
+          <Link
+            key={theater.id}
+            to={`/theaters/${theater.id}/my-seats`}
+            className="bg-gray-200 px-8 py-2 rounded-b-md transition-colors hover:bg-gray-300 hover:cursor-pointer"
+          >
+            {theater.theaterName}
+          </Link>
+        );
+      })}
+
+      <Link to={"save-new-seat"}>Add new seat</Link>
+    </main>
+  );
 }
