8ba9a9edb885400a1ecf6538727646cd89a3ad30
Christopher Mendez Linux <mendecj812@gmail.com>
2025-12-17 22:13:29 -0600

    chore: remove unused component

--- app/welcome/welcome.tsx
+++
@@ -1,9 +0,0 @@
-export function Welcome() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <h1>hello there</h1>
-      </div>
-    </main>
-  );
-}
